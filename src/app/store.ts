ee1486b42329cee3d025e98a9f93d18bee56ac58
mxrsel <marsel14082006@gmail.com>
2024-07-18 01:23:31 +0600

    added fetch request for shows and added extra reducers

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,7 +1,10 @@
 import {configureStore} from "@reduxjs/toolkit";
+import {showReducer} from "../store/ShowSlice.ts";
 
 const store = configureStore({
-    reducer: {}
+    reducer: {
+        show: showReducer
+    }
 });
 
 export type RootState = ReturnType<typeof store.getState>;
--- src/store/ShowSlice.ts
+++ src/store/ShowSlice.ts
@@ -1,4 +1,6 @@
-import {createSlice} from "@reduxjs/toolkit";
+import {createAsyncThunk, createSlice, PayloadAction} from "@reduxjs/toolkit";
+import {RootState} from "../app/store.ts";
+import axios from "axios";
 
 interface Show{
     id: string;
@@ -8,19 +10,44 @@ interface Show{
 }
 
 export interface ShowState {
-    shows: Show[];
+    show: Show[];
     loading: boolean;
     error: string | null;
 }
 
 const initialState: ShowState = {
-    shows: [],
+    show: [],
     loading: false,
     error: null,
 };
 
+const fetchShow = createAsyncThunk<Show[], string, { state: RootState }>(
+    'shows/fetchShows',
+    async (message: string) => {
+        const response = await axios.get<{ show: Show }[]>(`http://api.tvmaze.com/search/shows?q=${message}`);
+        return response.data.map(result => result.show);
+    }
+);
+
 const ShowSlice = createSlice({
     name: "show",
     initialState,
-    reducers: {}
+    reducers: {},
+    extraReducers: (builder) => {
+        builder
+            .addCase(fetchShow.pending, (state) => {
+                state.loading = true;
+                state.error = null;
+            })
+            .addCase(fetchShow.fulfilled, (state, action: PayloadAction<Show[]>) => {
+                state.loading = false;
+                state.show = action.payload;
+            })
+            .addCase(fetchShow.rejected, (state, action) => {
+                state.loading = false;
+                state.error = action.error.message || 'Failed';
+            })
+    }
 })
+
+export const showReducer = ShowSlice.reducer;
