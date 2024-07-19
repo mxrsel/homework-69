deafa4c0c94ab527f27935f8f94762a7bd175d09
mxrsel <marsel14082006@gmail.com>
2024-07-19 14:33:55 +0600

    added component autocomplete

--- src/components/Autocomplete.tsx
+++ src/components/Autocomplete.tsx
@@ -4,13 +4,13 @@ import { RootState } from '../app/store';
 import { Link } from 'react-router-dom';
 
 const Autocomplete: React.FC = () => {
-    const { shows, loading } = useSelector((state: RootState) => state.show);
+    const { show, loading } = useSelector((state: RootState) => state.show);
 
     if (loading) return <div>Loading</div>;
 
     return (
         <div className="autocomplete">
-            {shows.map((show) => (
+            {show.map((show) => (
                 <Link to={`/shows/${show.id}`} key={show.id}>
                     {show.name}
                 </Link>
