05efe4c98dd4da23f0666c5e040111edaba40d4b
mxrsel <marsel14082006@gmail.com>
2024-07-19 15:41:45 +0600

    created show for common functionality for the two components

---
+++ src/store/Show.tsx
@@ -0,0 +1,13 @@
+import SearchInput from "../components/SearchInput.tsx";
+import Autocomplete from "../components/Autocomplete.tsx";
+
+const Show = () => {
+    return (
+        <div>
+            <SearchInput />
+            <Autocomplete />
+        </div>
+    );
+};
+
+export default Show;
