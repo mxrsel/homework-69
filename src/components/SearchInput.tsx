bb8bb136df4e8f0870b51c45ceb7213343b7be2d
mxrsel <marsel14082006@gmail.com>
2024-07-19 15:07:58 +0600

    created component for input

---
+++ src/components/SearchInput.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const SearchInput: React.FC = () => {
+
+
+    return (
+        <input
+            type="text"
+            placeholder="Search for TV shows"
+        />
+    );
+};
+
+export default SearchInput;
