bf53961458496cc9c6d226d03265922024c280eb
mxrsel <marsel14082006@gmail.com>
2024-07-19 15:28:04 +0600

    finish component input for searching tv

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { AppDispatch } from '../app/store';
+import {fetchShow} from "../store/ShowSlice.ts";
 
 const SearchInput: React.FC = () => {
+    const [query, setQuery] = useState('');
+    const dispatch = useDispatch<AppDispatch>();
 
+    const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setQuery(event.target.value);
+        if (event.target.value) {
+            dispatch(fetchShow(event.target.value));
+        }
+    };
 
     return (
         <input
             type="text"
+            value={query}
+            onChange={handleInputChange}
             placeholder="Search for TV shows"
         />
     );
